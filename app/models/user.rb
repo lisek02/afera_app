4187f9cc37265d18cbbf47b04bc1a008fbafdd97
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-08-26 00:47:09 +0200

    continued work on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :login, presence: true
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+	validates :email, presence: true,
+										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end

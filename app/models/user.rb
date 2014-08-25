d1bbd9a3867a61f78a6b249c00d32389b19b80ae
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-08-25 16:00:39 +0200

    added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :login, presence: true
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+										uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+	
+	before { @user = User.new(login: "example", email: "user@example.com") }
+
+	subject { @user }
+
+	it { should respond_to(:login) }
+	it { should respond_to(:email) }
+
+	it { should be_valid }
+
+	describe "when login is not present" do
+		before { @user.login = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when email format is invalid" do
+		it "should be invalid" do
+			adresses = %w[user@foo,com user_at_foo.org, example.user@foo
+									foo@bar_baz.com foo@bar+baz.com]
+			adresses.each do |invalid_adress|
+				@user.email = invalid_adress
+				expect(@user).not_to be_valid
+			end
+		end
+	end
+
+	describe "when email format is valid" do
+		it "should be valid" do
+			adresses = %w[user@foo.COM A_US_ER@f.b.org first.lst@foo.jp a+b@baz.com]
+			adresses.each do |valid_adress|
+				@user.email = valid_adress
+				expect(@user).to be_valid
+			end
+		end
+	end
+
+	describe "when email address id already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it { should_not be_valid }
+	end
 end

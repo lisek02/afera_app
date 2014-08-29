6ee0b95aba39c81eb68a465e8cf61bac9dff3d29
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-08-30 00:21:58 +0200

    updating users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Authentication" do
+describe "Authentication", type: :request do
   subject { page }
 
   describe "signig page" do
@@ -21,26 +21,73 @@ describe "Authentication" do
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert') }
+        it { should_not have_selector('div.alert.alert-error') }
       end
     end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_title(user.login) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     
       describe "followed by signout" do
         before { click_link 'Sign out' }
         it { should have_link 'Sign in' }
+      end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          sign_in(user)
+        end
+
+        describe "after signin in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
+      describe "in User controller" do
+
+        describe "visiting edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title("Edit user")) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+
+      describe "submittin a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
       end
     end
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,4 +57,42 @@ describe "User pages" do
 			end
 		end
 	end
+
+	describe "edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in(user)
+			visit edit_user_path(user)
+		end
+
+		describe "page" do
+			it { should have_content("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/email') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_login) { "login" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Login",									with: new_login
+				fill_in "Email",									with: new_email
+				fill_in "Password",								with: user.password
+				fill_in "Password Confirmation", 	with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_login) }
+			it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+			specify { expect(user.reload.login).to eq new_login }
+			specify { expect(user.reload.email).to eq new_email }
+		end
+	end
 end
